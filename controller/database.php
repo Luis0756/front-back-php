98823af5002d956cd60e9580684c29678df65f21
renan-munhoz <renan.munhoz@edu.unifil.br>
2024-06-06 20:22:45 -0300

    ajustes database

--- controller/database.php
+++ controller/database.php
@@ -1,16 +1,32 @@
 <?php
-$servername = "localhost"; 
-$username = "root"; 
-$password = "root"; 
-$database = "root"; 
 
-$conn = new PDO("mysql:host={$servername};dbname={$database}");
+    class Database {
+        private string $servername; 
+        private $username; 
+        private $password; 
+        private $database; 
+        private $port; 
+        private PDO $PDO;
+        private function __construct() { 
+               
+            $this -> servername = "localhost";
+            $this -> username = "root";
+            $this -> password = "root";
+            $this -> database = "root";
+            $this -> port = "3306";
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+            $this -> PDO = new PDO("mysql:host={$this->servername};dbname={$this->servername};port={$this->port}");
+            
+            if ($this -> PDO->connect_error) {
+                die("Connection failed: " . $this -> PDO->connect_error);
+            }   
+            echo "Connected successfully";
+            
+            $this -> PDO->close();
+        }
 
-echo "Connected successfully";
-
-$conn->close();
+        public function Consultar() {
+            
+        }
+    }
 ?>
